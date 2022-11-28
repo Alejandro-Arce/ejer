5afffb10d8c7f11db07fc15d9532d44f1f3e9d1d
Alejandro-Arce <116038061+Alejandro-Arce@users.noreply.github.com>
2022-11-28 16:56:41 -0600

    Add files via upload

---
+++ cuento.py
@@ -0,0 +1,16 @@
+paginas = [
+    "erace una vez....."
+]
+
+
+
+text = " sfjalfhedfnedbgew9iugbeuibvegibe \n iueh fuehg "
+print(text)
+
+print("¿que quieres hacer?")
+
+print ("1...")
+print ("2...")
+print ("3...")
+
+respuesta = input()
