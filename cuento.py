f2e9b82a3c6bd6f95f04bb545c899500c08389a4
nicolas <bernalaj177@gmail.com>
2022-12-03 11:44:49 -0600

    se le agrega explicacion del codigo

--- cuento.py
+++ cuento.py
@@ -1,9 +1,13 @@
+#programa de libro con eleccion (el usuario va elijiendo el runbo de la historia)
+#primero comiensa en la pagina 0 y elige en cual continuar.
 paginas = [
     "Pagina 0 ) erace una vez... \n ¿Que quieres hacer? \n1... \n2... \n3...",
     "Pagina 1 ) erace una vez... \n ¿Que quieres hacer? \n1... \n2... \n3...",
     "Pagina 2 ) erace una vez... \n ¿Que quieres hacer? \n1... \n2... \n3...",
 ]
 
+#se define showPagina como el numero de pagina, el texto sera indicado por el numero de la pagina escogida se imprime la eleccion y...
+#el numero de la pagina escogida es conbertida a numero entero.
 
 def showPagina(NumPagina):
     text =paginas[NumPagina ]
